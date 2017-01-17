d42c2b4e14565f63e9c148c79a33bb5e68fa2825
catcamacho <m.catalina.camacho@gmail.com>
2017-01-17 10:41:20 -0800

    add fmri proc

--- preproc_restfmri.m
+++ preproc_restfmri.m
@@ -1,9 +1,61 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'/share/iang/active/BABIES/BABIES_rest/infant_rest/preproc_restfmri_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
+function preproc_restfmri
+filepath = '/share/iang/active/BABIES/BABIES_rest/subjDir/';
+subject = {''};
+
+spm_jobman('initcfg');
+for i = 1:length(subject)
+    fmri =  cellstr(spm_select('FPList',[filepath subject{1,i} '/rest'],'vol*'));
+    matlabbatch = preproc_restfmri_job(fmri);
+    spm('defaults', 'FMRI');
+    spm_jobman('run',matlabbatch);
 end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
+end
+
+function matlabbatch = preproc_restfmri_job(fmri)
+%-----------------------------------------------------------------------
+% Job saved on 17-Jan-2017 10:11:09 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (6685)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+%%
+matlabbatch{1}.spm.temporal.st.scans = {fmri}';
+%%
+matlabbatch{1}.spm.temporal.st.nslices = 35;
+matlabbatch{1}.spm.temporal.st.tr = 2.5;
+matlabbatch{1}.spm.temporal.st.ta = 2.43;
+matlabbatch{1}.spm.temporal.st.so = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35];
+matlabbatch{1}.spm.temporal.st.refslice = 18;
+matlabbatch{1}.spm.temporal.st.prefix = 'a';
+matlabbatch{2}.spm.spatial.realign.estwrite.data{1}(1) = cfg_dep('Slice Timing: Slice Timing Corr. Images (Sess 1)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.sep = 4;
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.interp = 2;
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.weight = '';
+matlabbatch{2}.spm.spatial.realign.estwrite.roptions.which = [2 1];
+matlabbatch{2}.spm.spatial.realign.estwrite.roptions.interp = 4;
+matlabbatch{2}.spm.spatial.realign.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{2}.spm.spatial.realign.estwrite.roptions.mask = 1;
+matlabbatch{2}.spm.spatial.realign.estwrite.roptions.prefix = 'r';
+matlabbatch{3}.spm.spatial.normalise.estwrite.subj.vol(1) = cfg_dep('Realign: Estimate & Reslice: Resliced Images (Sess 1)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','sess', '()',{1}, '.','rfiles'));
+matlabbatch{3}.spm.spatial.normalise.estwrite.subj.resample(1) = cfg_dep('Realign: Estimate & Reslice: Resliced Images (Sess 1)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','sess', '()',{1}, '.','rfiles'));
+matlabbatch{3}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
+matlabbatch{3}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
+matlabbatch{3}.spm.spatial.normalise.estwrite.eoptions.tpm = {'/home/catcam1/matlab/current/spm12/tpm/TPM.nii'};
+matlabbatch{3}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
+matlabbatch{3}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch{3}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
+matlabbatch{3}.spm.spatial.normalise.estwrite.eoptions.samp = 3;
+matlabbatch{3}.spm.spatial.normalise.estwrite.woptions.bb = [-78 -112 -70
+                                                             78 76 85];
+matlabbatch{3}.spm.spatial.normalise.estwrite.woptions.vox = [2 2 2];
+matlabbatch{3}.spm.spatial.normalise.estwrite.woptions.interp = 4;
+matlabbatch{3}.spm.spatial.normalise.estwrite.woptions.prefix = 'w';
+matlabbatch{4}.spm.spatial.smooth.data(1) = cfg_dep('Normalise: Estimate & Write: Normalised Images (Subj 1)', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+matlabbatch{4}.spm.spatial.smooth.fwhm = [4 4 4];
+matlabbatch{4}.spm.spatial.smooth.dtype = 0;
+matlabbatch{4}.spm.spatial.smooth.im = 0;
+matlabbatch{4}.spm.spatial.smooth.prefix = 's';
+end
