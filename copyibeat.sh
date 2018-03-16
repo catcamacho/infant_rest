3d8dacc5143e7edb44a702effb2d548da5dc276a
Maria Catalina Camacho <m.catalina.camacho@gmail.com>
2018-03-16 13:07:50 -0700

    fix copy ibeat paths

--- copyibeat.sh
+++ copyibeat.sh
@@ -1,7 +1,7 @@
-#! /bin/csh
+#! /bin/tcsh
 
-set ibeat_dir = /Volumes/iang/active/BABIES/BABIES_ibeat/subjDir/BABIES_C
-set analysis_dir = /Volumes/iang/active/BABIES/BABIES_rest/subjDir
+set ibeat_dir = /share/iang/active/BABIES/BABIES_ibeat/subjDir/BABIES_C
+set analysis_dir = /share/iang/active/BABIES/BABIES_rest/subjDir
 set log = $analysis_dir/ibeatlog.txt
 
 foreach sub ($argv)
@@ -17,10 +17,10 @@ if (-e $ibeat_dir/T1${sub}/T1${sub}-5) then
 	mri_convert --in_orientation RAS *T2-reoriented-strip.img skullstripped_anat.nii
 	#mri_convert --in_orientation RAS *seg-aal.img aal_segmentation.nii
 	
-	cp *.nii $analysis_dir/$fldr/anat/	
+	cp skullstripped_anat.nii $analysis_dir/$fldr/	
 endif
 
-if (-e $analysis_dir/$fldr/anat/skullstripped_anat.nii) then
+if (-e $analysis_dir/$fldr/skullstripped_anat.nii) then
 	echo '---------------- iBEAT anats successfully copied for '$sub >> $log
 else
 	echo 'ERROR: iBEAT directory not found for '$sub >> $log
